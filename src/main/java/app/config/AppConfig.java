73f24642ce5ee72aceb7f6ebc39e062633ff1d96
prozorovmy <miha.prozorov1@gmail.com>
2024-09-30 17:45:05 +0300

    Выполнена задача 2.1.3 Spring Beans

--- src/main/java/app/config/AppConfig.java
+++ src/main/java/app/config/AppConfig.java
@@ -1,5 +1,7 @@
 package app.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
